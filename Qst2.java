da027a09c2f41f1c01a045a187bc73445fd084ee
j-ami-l <mdhasanjamil0001@gmail.com>
2025-09-12 10:07:03 +0600

    Added qst 2 ans

---
+++ Qst2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Qst2 {
+    public static void main(String[] args) {
+        System.out.println("Enter Your Name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
--- helloWorld.java
+++ helloWorld.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class helloWorld {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
         System.out.println("Hello World");
     }
 }
